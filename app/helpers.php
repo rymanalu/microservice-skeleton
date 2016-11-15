4ffcda4d420fb9c11ddca2f5342b626cba71eb48
Roni Yusuf Manalu <rymanalu@gmail.com>
2016-11-15 10:03:02 +0700

    Add transaction helper & update dependencies.

--- app/helpers.php
+++ app/helpers.php
@@ -12,6 +12,24 @@
  |
  */
 
+if (! function_exists('log_exception')) {
+    /**
+     * Log the given exception.
+     *
+     * @param  \Exception  $exception
+     * @return void
+     */
+    function log_exception(Exception $exception)
+    {
+        Log::error(
+            'Message: '.$exception->getMessage().PHP_EOL.
+            'Code: '.$exception->getCode().PHP_EOL.
+            'File: '.$exception->getFile().PHP_EOL.
+            'Line: '.$exception->getLine()
+        );
+    }
+}
+
 if (! function_exists('response_json')) {
     /**
      * Return a new JSON response from the application.
@@ -26,16 +44,43 @@ if (! function_exists('response_json')) {
     {
         $isSuccessful = $status >= 200 && $status < 300;
 
-        $defaultMessage = $isSuccessful  ? 'OK' : 'Failed';
+        $defaultMessage = $isSuccessful ? 'OK' : 'Failed';
 
         $data = array_merge(array_except($data, 'message'), [
             'status' => [
                 'message' => isset($data['message']) ? $data['message'] : $defaultMessage,
                 'succeded' => $isSuccessful,
                 'code' => $status,
-            ]
+            ],
         ]);
 
         return response()->json($data, $status, $headers, $options);
+    }
+}
+
+if (! function_exists('transaction')) {
+    /**
+     * Execute a Closure within a transaction.
+     *
+     * @param  \Closure  $callback
+     * @return mixed
+     */
+    function transaction(Closure $callback)
+    {
+        DB::beginTransaction();
+
+        try {
+            $result = $callback();
+
+            DB::commit();
+        } catch (Exception $e) {
+            DB::rollBack();
+
+            log_exception($e);
+
+            $result = false;
+        }
+
+        return $result;
     }
 }
